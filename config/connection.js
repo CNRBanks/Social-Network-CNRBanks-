a56f9e90c7b57bcce9f18408f1a2e099b7113d08
unknown <connortamminen@gmail.com>
2022-10-26 20:32:31 -0500

    updated index in models, added gitignore, and updated the connection js file with the correct connect address

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-connect('mongodb://localhost/videosAndResponses', {
+mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkCNRBanks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
